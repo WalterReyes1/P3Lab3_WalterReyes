70d1b7922f5202d2e2c49a89d571b9496b3c7b03
walter reyes <walter.reyes@cambridgehn.com>
2021-05-07 16:21:58 -0600

    1

--- main.cpp
+++ main.cpp
@@ -1,10 +1,60 @@
 #include <iostream>
-
+#include <string>
+#include <stdlib.h> 
+#include <ctime>
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+
 int opcion = 0;
 int menu();
+char* ejercicio1(char*,int);
+char** ejercicio2(int,int,int);
+void freeMatriz(char**&, int);
+int main() {
 
-int main(int argc, char** argv) {
+	while(opcion != 4) {
+		switch(opcion=menu()) {
+			case 1: {
+				char cadena[1000];
+				int t = 0;
+				do{
+				cout<<"Ingrese el tamaño de la cadena: ";
+				cin>>t;					
+				}while(t <0 || t>1000);
+				char* a = new char[t];
+				for(int i = 0; i < t;i++){
+					cout<<"Ingrese caracter: ";
+					cin>>cadena;
+					a[i]=cadena[i];
+				}
+				char* b =ejercicio1(a,t);
+				break;
+			}
+			case 2: {
+				int filas =0;
+				int columnas=0;
+				int k = 0;
+				cout<<"Ingrese filas: ";
+				cin>>filas;
+				cout<<"Ingrese Columnas: ";
+				cin>>columnas;
+				cout<<"Ingrese bombas: ";
+				cin>>k;
+				char** a = ejercicio2(filas,columnas,k);
+				freeMatriz(a,filas);
+				break;
+			}
+			case 3: {
+
+				break;
+			}
+			case 4: {
+				cout<<"Salir"<<endl;
+				break;
+			}
+		}//fin switch
+
+	}// fin while menu
 	return 0;
 }
 
@@ -14,13 +64,82 @@ int menu() {
 		cout<<"1.Ejercicio 1"<<endl;
 		cout<<"2.Ejercicio 2"<<endl;
 		cout<<"3.Ejercicio 3"<<endl;
-		cout<<"4.Ejercicio 4"<<endl;
-		cout<<"5.Salir"<<endl;
+		cout<<"4.Salir"<<endl;
 		cout<<"Eliga una opcion: ";
 		cin>>opcion;
-		if(opcion >0 && opcion <6) {
+		if(opcion >0 && opcion <5) {
 			return opcion;
 		}
 
 	}
 }
+
+char* ejercicio1(char* a,int size){	
+	int valor_actual = 0;
+	int f = 0;
+	for(int i = 0; i<size;i++){		
+		
+		if( (a[i]- '0')>0 &&  (a[i]- '0')<=9){
+			
+			int cont = i+1;
+			while(a[cont]-'0'>0 && a[cont]-'0'<=9){
+				//if()	
+			cont++;
+			}
+				
+		}
+				
+	}
+	return a;
+}
+char** ejercicio2(int fila, int columna, int k){
+	char** retval = new char* [fila];
+	for(int i = 0; i<fila;i++){
+		retval[i]=new char[columna];
+	}
+	
+	for(int i = 0; i<fila;i++){
+		for(int j = 0; j<columna;j++){
+			retval[i][j]='-';
+		}
+	}
+	
+	for(int i = 0; i<fila;i++){
+		for(int j = 0; j<columna;j++){
+			while(k>0){
+				srand(time(NULL));
+				int x= 1+rand() % fila-1;
+				srand(time(NULL));
+				int y=1+rand()% columna-1;
+				if(retval[x][y]=='-'){
+					retval[x][y]='#';
+					k--;
+				}
+			}
+		}
+	}
+	
+	for(int i = 0; i<fila;i++){
+		cout<<"| ";
+		for(int j = 0; j<columna;j++){
+			cout<<retval[i][j]<<" ";;
+		}
+		cout<<" |";
+		cout<<endl;
+	}
+	cout<<endl;
+	return retval; 
+}
+		
+void freeMatriz(char**& m,int fila){
+	if(m!=NULL){
+		for(int i = 0; i<fila;i++){
+			if(m[i] != NULL){
+				delete[] m[i];
+				m[i]=NULL;
+			}
+		}
+		delete[] m;
+		m= NULL;	
+	}
+}	
